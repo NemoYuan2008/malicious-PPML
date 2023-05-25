7ebedfefcbfa4a70c9da059cf7d05aff55a85fed
Boshi Yuan <542629877@qq.com>
2023-05-25 20:23:52 +0800

    Add OperationOffline

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include "share/Share.h"
 #include "share/Spdz2kShare.h"
 #include "offline/FakeOffline.h"
+#include "protocols/OperationOffline.h"
 
 int main() {
     Spdz2kShare64 s(12, 23), k(2, 10);
     auto t = s + k;
 
-
+    AdditionOffline<64, 64> a(s, k);
     return 0;
 }
---
+++ src/protocols/OperationOffline.h
@@ -0,0 +1,42 @@
+#ifndef MALICIOUS_PPML_OPERATIONOFFLINE_H
+#define MALICIOUS_PPML_OPERATIONOFFLINE_H
+
+#include "share/Spdz2kShare.h"
+
+template<int K, int S>
+class OperationOffline {
+public:
+    OperationOffline(const Spdz2kShare<K, S> &p_delta_z) : delta_z(p_delta_z) {}
+
+    virtual void runOffline(const Spdz2kShare<K, S> &delta_x, const Spdz2kShare<K, S> &delta_y) = 0;
+
+protected:
+    Spdz2kShare<K, S> delta_z;
+};
+
+
+template<int K, int S>
+class AdditionOffline : public OperationOffline<K, S> {
+public:
+    AdditionOffline(const Spdz2kShare<K, S> &p_delta_z) : OperationOffline<K, S>(p_delta_z) {}
+
+    void runOffline(const Spdz2kShare<K, S> &delta_x, const Spdz2kShare<K, S> &delta_y) override {
+        this->delta_z = delta_x + delta_y;
+    }
+};
+
+
+template<int K, int S>
+class MultiplicationOffline : public OperationOffline<K, S> {
+protected:
+    Spdz2kShare<K, S> delta_xy;
+public:
+    MultiplicationOffline(const Spdz2kShare<K, S> &p_delta_z, const Spdz2kShare<K, S> &p_delta_xy)
+            : OperationOffline<K, S>(p_delta_z), delta_xy(p_delta_xy) {}
+
+    void runOffline(const Spdz2kShare<K, S> &delta_x, const Spdz2kShare<K, S> &delta_y) override {
+//        this->delta_xy =
+    }
+};
+
+#endif //MALICIOUS_PPML_OPERATIONOFFLINE_H
