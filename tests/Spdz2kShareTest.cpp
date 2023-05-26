389ed09ec628407afc9efbae0b593c8ed9358e9d
Boshi Yuan <542629877@qq.com>
2023-05-26 14:51:55 +0800

    Add tests for Spdz2kShare.h

--- tests/Spdz2kShareTest.cpp
+++ tests/Spdz2kShareTest.cpp
@@ -6,6 +6,15 @@
 
 BOOST_AUTO_TEST_SUITE(Spdz2kTest)
 
+    BOOST_AUTO_TEST_CASE(Size) {
+        BOOST_CHECK_EQUAL(sizeof(Spdz2kShare32::KSType), sizeof(uint64_t));
+        BOOST_CHECK_EQUAL(sizeof(Spdz2kShare32::KType), sizeof(uint32_t));
+        BOOST_CHECK_EQUAL(sizeof(Spdz2kShare32::SType), sizeof(uint32_t));
+        BOOST_CHECK_EQUAL(sizeof(Spdz2kShare64::KSType), sizeof(__uint128_t));
+        BOOST_CHECK_EQUAL(sizeof(Spdz2kShare64::KType), sizeof(uint64_t));
+        BOOST_CHECK_EQUAL(sizeof(Spdz2kShare64::SType), sizeof(uint64_t));
+    }
+
     BOOST_AUTO_TEST_CASE(Addition) {
         BOOST_CHECK_EQUAL(Spdz2kShare32(12, 34) + Spdz2kShare32(21, 43), Spdz2kShare32(33, 77));
     }
