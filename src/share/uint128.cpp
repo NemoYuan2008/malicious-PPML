df7ecc297d7fa108c9d504d35953a8e3ddb73c51
Boshi Yuan <542629877@qq.com>
2023-05-26 14:19:35 +0800

    Add uint128 operations

---
+++ src/share/uint128.cpp
@@ -0,0 +1,19 @@
+//
+// Created by yuan on 23-5-26.
+//
+
+#include "uint128.h"
+
+using std::ostream;
+using std::setfill;
+using std::setw;
+using std::hex;
+using std::dec;
+
+ostream &operator<<(ostream &os, __uint128_t x) {
+    os << hex
+       << setfill('0') << setw(16) << static_cast<uint64_t>(x >> 64)
+       << setfill('0') << setw(16) << static_cast<uint64_t>(x)
+       << dec;
+    return os;
+}
---
+++ src/share/uint128.h
@@ -0,0 +1,11 @@
+#ifndef MALICIOUS_PPML_UINT128_H
+#define MALICIOUS_PPML_UINT128_H
+
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+
+std::ostream &operator<<(std::ostream &os, __uint128_t x);
+
+
+#endif //MALICIOUS_PPML_UINT128_H
