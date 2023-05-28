90be150a535b8278b63dbbc12f86cd638abc0baa
Boshi Yuan <542629877@qq.com>
2023-05-28 10:25:54 +0800

    Add addition and multiplication gate

--- src/protocols/Gate.h
+++ src/protocols/Gate.h
@@ -11,8 +11,32 @@ public:
     using SType = SType_t<S>;
     using KSType = KSType_t<K, S>;
 
+    virtual void runOffline(const Spdz2kShare<K, S> &delta_x, const Spdz2kShare<K, S> &delta_y) = 0;
+
 protected:
-    KType
+    Spdz2kShare<K, S> delta_z;
+};
+
+
+template<int K, int S>
+class AdditionGate : public Gate<K, S> {
+public:
+    virtual void runOffline(const Spdz2kShare<K, S> &delta_x, const Spdz2kShare<K, S> &delta_y) override {
+        this->delta_z = delta_x + delta_y;
+    }
+};
+
+
+template<int K, int S>
+class MultiplicationGate : public Gate<K, S> {
+public:
+
+    void runOffline(const Spdz2kShare<K, S> &delta_x, const Spdz2kShare<K, S> &delta_y) override {
+//        this->delta_xy =
+    }
+
+protected:
+    Spdz2kShare<K, S> delta_xy;
 };
 
 
