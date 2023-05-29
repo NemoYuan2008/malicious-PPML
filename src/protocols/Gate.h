27f34e5129638d6d99a75acc12a3a505d7ce745b
Boshi Yuan <542629877@qq.com>
2023-05-29 10:46:30 +0800

    Change Gate template parameter

--- src/protocols/Gate.h
+++ src/protocols/Gate.h
@@ -5,13 +5,9 @@
 #include <memory>
 #include "protocols/GateOffline.h"
 
-template<int K, int S>
+template<typename ShrType>
 class Gate {
 public:
-    using KType = KType_t<K>;
-    using SType = SType_t<S>;
-    using KSType = KSType_t<K, S>;
-
     Gate() = default;
 
     Gate(const std::shared_ptr<Gate> &input_x, const std::shared_ptr<Gate> &input_y)
@@ -19,17 +15,17 @@ public:
 
     virtual void runOffline() = 0;
 
-    const Spdz2kShare<K, S> &getLambda_zShr() const { return lambda_zShr; }  //TODO: return ref of value?
+    const ShrType &getLambda_zShr() const { return lambda_zShr; }  //TODO: return ref of value?
 
 protected:
     //Maybe: define clearLambda here for debugging purpose
-    Spdz2kShare<K, S> lambda_zShr;
+    ShrType lambda_zShr;
     std::shared_ptr<Gate> input_x, input_y;
 };
 
 
-template<int K, int S>
-class AdditionGate : public Gate<K, S> {
+template<typename ShareType>
+class AdditionGate : public Gate<ShareType> {
 public:
     void runOffline() override {
         this->lambda_zShr = this->input_x->getLambda_zShr() + this->input_y->getLambda_zShr();
@@ -37,8 +33,8 @@ public:
 };
 
 
-template<int K, int S>
-class MultiplicationGate : public Gate<K, S> {
+template<typename ShareType>
+class MultiplicationGate : public Gate<ShareType> {
 public:
     void runOffline() override {
         // generate random lambda_zShr
@@ -46,7 +42,7 @@ public:
     }
 
 protected:
-    Spdz2kShare<K, S> lambda_xyShr;
+    ShareType lambda_xyShr;
 };
 
 
--- src/protocols/GateOffline.cpp
+++
@@ -1,1 +0,0 @@
-#include "GateOffline.h"
