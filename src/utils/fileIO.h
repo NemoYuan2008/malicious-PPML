140b1eab4606c00f2ed3dbde48c1b3911d873b7d
Boshi Yuan <542629877@qq.com>
2023-06-02 20:49:53 +0800

    Add fileIO.h

--- src/utils/fileIO.h
+++ src/utils/fileIO.h
@@ -15,6 +15,7 @@ openOutputFiles(const std::array<std::string, N> &fileNames) {
     std::array<std::unique_ptr<std::ostream>, N> ret;
     for (size_t i = 0; i < N; ++i) {
         ret[i] = std::make_unique<std::ofstream>(fileNames[i], std::ios_base::out);
+        *ret[i] << std::hex;
     }
     return ret;
 }
