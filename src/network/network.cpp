a7e8f129ff13d9a07375b329d05d57ee594b7a6d
Boshi Yuan <542629877@qq.com>
2023-06-04 23:14:39 +0800

    cleanup code

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include <random>
-#include <cstddef>
-#include <boost/asio.hpp>
 #include <iomanip>
+#include <cstring>
 #include <boost/thread.hpp>
-#include <fstream>
-#include <string.h>
 #include "network.h"
 
 using namespace boost::asio;
@@ -14,11 +10,10 @@ using std::cout;
 using std::endl;
 using std::array;
 
-Network::Network (uint32_t id, uint16_t port, uint16_t secLevel)
-        : id(id), port(port), secLevel(secLevel)
-        {
- //   io_service ios;
-    
+Network::Network(uint32_t id, uint16_t port, uint16_t secLevel)
+        : id(id), port(port), secLevel(secLevel) {
+    //   io_service ios;
+
 //    if (id==1)
 //    {
 //        tcp::socket *socket = new tcp::socket (ios);
@@ -39,15 +34,15 @@ Network::Network (uint32_t id, uint16_t port, uint16_t secLevel)
 
 }
 
-void Network::close(){
-    for(size_t i=0; i < sockets.size(); ++i){
+void Network::close() {
+    for (size_t i = 0; i < sockets.size(); ++i) {
         tcp::socket *socket = sockets.at(i);
         socket->shutdown(socket_base::shutdown_both);
         //socket->close();
     }
 }
-bool Network::send(uint party_id, const uint8_t *buf, uint32_t numBytes)
-{
+
+bool Network::send(uint party_id, const uint8_t *buf, uint32_t numBytes) {
     uint socket_id = party_id;
     if (party_id > sockets.size()) {
         std::cerr << "send failed! server with server id " << party_id << " is not added" << endl;
@@ -59,24 +54,23 @@ bool Network::send(uint party_id, const uint8_t *buf, uint32_t numBytes)
     //sockets[socket_id]->write_some(buffer(buf, numBytes), err);
     if (!err) {
         return true;
-    }
-    else {
+    } else {
         cout << "send failed: " << err.message() << endl;
         return false;
     }
 }
-bool Network::rcv(uint party_id, uint8_t *buf, uint32_t numBytes)
-{
+
+bool Network::rcv(uint party_id, uint8_t *buf, uint32_t numBytes) {
     if (party_id > sockets.size()) {
         std::cerr << "receive failed! server with player id " << party_id << " is not added" << endl;
         return false;
     }
     uint socket_id = party_id;
-    if(party_id >= id) socket_id--;
+    if (party_id >= id) socket_id--;
     streambuf recBuff;
     boost::system::error_code err;
-    read (*sockets[socket_id], recBuff, transfer_at_least(numBytes), err);
-    if (err && err!= error::eof) {
+    read(*sockets[socket_id], recBuff, transfer_at_least(numBytes), err);
+    if (err && err != error::eof) {
         std::cerr << "receive failed: " << err.message() << endl;
         return false;
     } else {
@@ -84,24 +78,25 @@ bool Network::rcv(uint party_id, uint8_t *buf, uint32_t numBytes)
         return true;
     }
 }
-const uint32_t Network::getSeclevel() { return this->secLevel; }
-const uint32_t Network::getID() { return this->id; }
-const char* Network::getaddr() { return this->addr; }
+
+uint32_t Network::getSeclevel() const { return this->secLevel; }
+
+uint32_t Network::getID() const { return this->id; }
+
+const char *Network::getAddr() { return this->addr; }
 
 void Network::connect() {
     io_service ios;
-    if (id==1)
-    {
-        tcp::socket *socket = new tcp::socket (ios);
-        cout << "endpoint: "<<port<<endl;
+    if (id == 1) {
+        tcp::socket *socket = new tcp::socket(ios);
+        cout << "endpoint: " << port << endl;
         socket->connect(tcp::endpoint(ip::address::from_string(addr), port));
         sockets.push_back(socket);
-        cout<< "connected"<<endl;
-    }
-    else{
-        cout << "endpoint: "<<port<<endl;
-        ip::tcp::acceptor acceptor(ios,tcp::endpoint(ip::address::from_string(addr), port));
-        tcp::socket *socket = new tcp::socket (ios);
+        cout << "connected" << endl;
+    } else {
+        cout << "endpoint: " << port << endl;
+        ip::tcp::acceptor acceptor(ios, tcp::endpoint(ip::address::from_string(addr), port));
+        tcp::socket *socket = new tcp::socket(ios);
         acceptor.accept(*socket);
         sockets.push_back(socket);
         //std::cout << socket->remote_endpoint().address() << std::endl;
--- src/network/network.h
+++ src/network/network.h
@@ -1,4 +1,3 @@
-
 #include <cstddef>
 #include <iostream>
 #include <iomanip>
@@ -7,35 +6,36 @@
 #include <boost/bind.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
-using namespace boost::asio;
-using ip::tcp;
-using std::cout;
-using std::endl;
-using std::array;
 
+class Network {
 
+public:
+    Network(uint32_t id, uint16_t port = 7766, uint16_t secLevel = 128);
 
-class Network{
+    uint32_t getSeclevel() const;
 
- public:
-  Network (uint32_t id, uint16_t port = 7766, uint16_t secLevel = 128);
-  const uint32_t getSeclevel();
-  const uint32_t getID();
-  const char* getaddr();
-  bool send(uint party_id, const uint8_t *buf, uint32_t numBytes);
-  bool rcv(uint party_id, uint8_t *buf, uint32_t numBytes);
-  uint16_t nparties = 2;
-  void connect();
-  void close(); // close the sockets
+    uint32_t getID() const;
 
- private:
-  const uint32_t id;
-  const uint16_t port;
-  const char* addr = "127.0.0.1";
-  const uint16_t secLevel;
-  std::vector<tcp::socket*> sockets;
-  uint64_t counter = 0;
-  boost::mutex mutex;
+    const char *getAddr();
+
+    bool send(uint party_id, const uint8_t *buf, uint32_t numBytes);
+
+    bool rcv(uint party_id, uint8_t *buf, uint32_t numBytes);
+
+    uint16_t nparties = 2;
+
+    void connect();
+
+    void close(); // close the sockets
+
+private:
+    const uint32_t id;
+    const uint16_t port;
+    const char *addr = "127.0.0.1";
+    const uint16_t secLevel;
+    std::vector<boost::asio::ip::tcp::socket *> sockets;
+    uint64_t counter = 0;
+    boost::mutex mutex;
 //  bool send(uint party_id,const uint8_t *buf, uint32_t numBytes);
 //  bool rcv(uint party_id,const uint8_t *buf, uint32_t numBytes);
 };
