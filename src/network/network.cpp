97a3f7e84665b7878f46a08fa910e0f58bea5d0a
Boshi Yuan <542629877@qq.com>
2023-06-05 16:47:31 +0800

    improve network

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -11,7 +11,7 @@ using std::endl;
 using std::array;
 
 Network::Network(uint32_t id, uint16_t port)
-    : id(id), port(port) {
+        : id(id), port(port) {
     //   io_service ios;
 
 //    if (id==1)
@@ -43,6 +43,7 @@ void Network::close() {
 }
 
 bool Network::send(uint party_id, const uint8_t *buf, uint32_t numBytes) {
+
     uint socket_id = party_id;
     if (party_id > sockets.size()) {
         std::cerr << "send failed! server with server id " << party_id << " is not added" << endl;
@@ -50,6 +51,7 @@ bool Network::send(uint party_id, const uint8_t *buf, uint32_t numBytes) {
     }
     if (party_id >= id) socket_id--;
     boost::system::error_code err;
+    cout << "start sending" << endl;
     write(*sockets[socket_id], buffer(buf, numBytes), err);
     //sockets[socket_id]->write_some(buffer(buf, numBytes), err);
     if (!err) {
@@ -60,13 +62,60 @@ bool Network::send(uint party_id, const uint8_t *buf, uint32_t numBytes) {
     }
 }
 
-bool Network::rcv(uint party_id, uint8_t *buf, uint32_t numBytes) {
-    if (party_id > sockets.size()) {
-        std::cerr << "receive failed! server with player id " << party_id << " is not added" << endl;
+bool Network::send(uint party_id, uint64_t *data) {
+    uint socket_id = party_id;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal" << endl;
         return false;
     }
+    if (party_id > id) socket_id--;
+    boost::system::error_code err;
+    // uint64 --> char*
+    uint8_t *buf;
+    buf = (uint8_t *) malloc(8);
+    memcpy(buf, data, 8);
+    write(*sockets[socket_id], buffer(buf, 8), err);
+    //sockets[socket_id]->write_some(buffer(buf, numBytes), err);
+    free(buf);
+    if (!err) {
+        return true;
+    } else {
+        cout << "send failed: " << err.message() << endl;
+        return false;
+    }
+}
+
+bool Network::send(uint party_id, __uint128_t *data) {
     uint socket_id = party_id;
-    if (party_id >= id) socket_id--;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal" << endl;
+        return false;
+    }
+    if (party_id > id) socket_id--;
+    boost::system::error_code err;
+    // uint64 --> char*
+    uint8_t *buf = (uint8_t *) malloc(16);
+    uint8_t *temp;
+    memcpy(buf, data, 16);
+    write(*sockets[socket_id], buffer(buf, 16), err);
+    free(buf);
+    //sockets[socket_id]->write_some(buffer(buf, numBytes), err);
+    if (!err) {
+        return true;
+    } else {
+        cout << "send failed: " << err.message() << endl;
+        return false;
+    }
+}
+
+
+bool Network::rcv(uint party_id, uint8_t *buf, uint32_t numBytes) {
+    uint socket_id = party_id;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal" << endl;
+        return false;
+    }
+    if (party_id > id) socket_id--;
     streambuf recBuff;
     boost::system::error_code err;
     read(*sockets[socket_id], recBuff, transfer_at_least(numBytes), err);
@@ -78,6 +127,47 @@ bool Network::rcv(uint party_id, uint8_t *buf, uint32_t numBytes) {
         return true;
     }
 }
+
+bool Network::rcv(uint party_id, uint64_t *data) {
+    uint socket_id = party_id;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal" << endl;
+        return false;
+    }
+    if (party_id > id) socket_id--;
+    streambuf recBuff;
+    boost::system::error_code err;
+    read(*sockets[socket_id], recBuff, transfer_at_least(8), err);
+    if (err && err != error::eof) {
+        std::cerr << "receive failed: " << err.message() << endl;
+        return false;
+    } else {
+        memcpy(data, buffer_cast<const void *>(recBuff.data()), 8);
+        cout << "received. " << endl;
+        return true;
+    }
+}
+
+bool Network::rcv(uint party_id, __uint128_t *data) {
+    uint socket_id = party_id;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal" << endl;
+        return false;
+    }
+    if (party_id > id) socket_id--;
+    streambuf recBuff;
+    boost::system::error_code err;
+    read(*sockets[socket_id], recBuff, transfer_at_least(16), err);
+    if (err && err != error::eof) {
+        std::cerr << "receive failed: " << err.message() << endl;
+        return false;
+    } else {
+        memcpy(data, buffer_cast<const void *>(recBuff.data()), 16);
+        cout << "received." << endl;
+        return true;
+    }
+}
+
 
 uint32_t Network::getID() const { return this->id; }
 
--- src/network/network.h
+++ src/network/network.h
@@ -18,7 +18,21 @@ public:
 
     bool send(uint party_id, const uint8_t *buf, uint32_t numBytes);
 
+    bool send(uint party_id, uint64_t *data);
+
+    bool send(uint party_id, __uint128_t *data);
+
+    template<typename T>
+    bool send(uint party_id, T *data);
+
     bool rcv(uint party_id, uint8_t *buf, uint32_t numBytes);
+
+    bool rcv(uint party_id, uint64_t *data);
+
+    bool rcv(uint party_id, __uint128_t *data);
+
+    template<typename T>
+    bool rcv(uint party_id, T *data);
 
     uint16_t nparties = 2;
 
@@ -36,4 +50,51 @@ private:
 //  bool send(uint party_id,const uint8_t *buf, uint32_t numBytes);
 //  bool rcv(uint party_id,const uint8_t *buf, uint32_t numBytes);
 };
+
+
+template<typename T>
+bool Network::send(uint party_id, T *data) {
+    uint socket_id = party_id;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal\n";
+        return false;
+    }
+    if (party_id > id) socket_id--;
+    boost::system::error_code err;
+    // uint64 --> char*
+    uint8_t *buf = (uint8_t *) malloc(sizeof(T));
+    uint8_t *temp;
+    memcpy(buf, data, sizeof(T));
+    write(*sockets[socket_id], boost::asio::buffer(buf, sizeof(T)), err);
+    free(buf);
+    //sockets[socket_id]->write_some(buffer(buf, numBytes), err);
+    if (!err) {
+        return true;
+    } else {
+        std::cout << "send failed: " << err.message() << '\n';
+        return false;
+    }
+}
+
+
+template<typename T>
+bool Network::rcv(uint party_id, T *data) {
+    uint socket_id = party_id;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal" << '\n';
+        return false;
+    }
+    if (party_id > id) socket_id--;
+    boost::asio::streambuf recBuff;
+    boost::system::error_code err;
+    read(*sockets[socket_id], recBuff, boost::asio::transfer_at_least(sizeof(T)), err);
+    if (err && err != boost::asio::error::eof) {
+        std::cerr << "receive failed: " << err.message() << '\n';
+        return false;
+    } else {
+        memcpy(data, boost::asio::buffer_cast<const void *>(recBuff.data()), sizeof(T));
+        std::cout << "received.\n";
+        return true;
+    }
+}
 
