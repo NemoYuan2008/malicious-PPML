3b5246f928cb94c42b676f63ddf5f9eed91caa92
voidwings <1512229747@qq.com>
2023-06-06 02:48:08 -0700

    Complete template send, rcv. Complete multi-player communication.

--- src/networking/Network.h
+++ src/networking/Network.h
@@ -1,6 +1,6 @@
 #ifndef MALICIOUS_PPML_NETWORK_H
 #define MALICIOUS_PPML_NETWORK_H
-
+#define STATE
 
 #include <cstddef>
 #include <iostream>
@@ -24,8 +24,14 @@ public:
 
     bool send(uint party_id, __uint128_t *data);
 
+//    template<typename T>
+//    bool send(uint party_id, T *data);
+
     template<typename T>
-    bool send(uint party_id, T *data);
+    bool send(uint party_id, const T &data);
+
+    template<typename T>
+    bool send(uint party_id, const std::vector<T> &data);
 
     bool rcv(uint party_id, uint8_t *buf, uint32_t numBytes);
 
@@ -36,9 +42,14 @@ public:
     template<typename T>
     bool rcv(uint party_id, T *data);
 
+    template<typename T>
+    bool rcv(uint party_id, std::vector<T> *data, uint32_t numElements);
+
     uint16_t nparties = 2;
 
     void connect();
+
+    void ConnectTo(uint32_t partyid);
 
     void close(); // close the sockets
 
@@ -49,13 +60,38 @@ private:
     std::vector<boost::asio::ip::tcp::socket *> sockets;
     uint64_t counter = 0;
     boost::mutex mutex;
+    boost::mutex printmutex;
 //  bool send(uint party_id,const uint8_t *buf, uint32_t numBytes);
 //  bool rcv(uint party_id,const uint8_t *buf, uint32_t numBytes);
 };
 
 
+//template<typename T>
+//bool Network::send(uint party_id, T *data) {
+//    uint socket_id = party_id;
+//    if (party_id > sockets.size() || party_id == id) {
+//        std::cerr << "send failed! server id " << party_id << " is not legal\n";
+//        return false;
+//    }
+//    if (party_id > id) socket_id--;
+//    boost::system::error_code err;
+//    // uint64 --> char*
+//    uint8_t *buf = (uint8_t *) malloc(sizeof(T));
+//    uint8_t *temp;
+//    memcpy(buf, data, sizeof(T));
+//    write(*sockets[socket_id], boost::asio::buffer(buf, sizeof(T)), err);
+//    free(buf);
+//    //sockets[socket_id]->write_some(buffer(buf, numBytes), err);
+//    if (!err) {
+//        return true;
+//    } else {
+//        std::cout << "send failed: " << err.message() << '\n';
+//        return false;
+//    }
+//}
+
 template<typename T>
-bool Network::send(uint party_id, T *data) {
+bool Network::send(uint party_id, const T &data) {
     uint socket_id = party_id;
     if (party_id > sockets.size() || party_id == id) {
         std::cerr << "send failed! server id " << party_id << " is not legal\n";
@@ -66,7 +102,7 @@ bool Network::send(uint party_id, T *data) {
     // uint64 --> char*
     uint8_t *buf = (uint8_t *) malloc(sizeof(T));
     uint8_t *temp;
-    memcpy(buf, data, sizeof(T));
+    memcpy(buf, &data, sizeof(T));
     write(*sockets[socket_id], boost::asio::buffer(buf, sizeof(T)), err);
     free(buf);
     //sockets[socket_id]->write_some(buffer(buf, numBytes), err);
@@ -78,6 +114,34 @@ bool Network::send(uint party_id, T *data) {
     }
 }
 
+template<typename T>
+bool Network::send(uint party_id, const std::vector<T> &data) {
+    uint socket_id = party_id;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal\n";
+        return false;
+    }
+    if (party_id > id) socket_id--;
+    boost::system::error_code err;
+    // uint64 --> char*
+    uint32_t numBytes;
+    numBytes = sizeof(T) * data.size();
+    if (numBytes == 0) {
+        std::cout << "the vector is empty!\n";
+        return false;
+    }
+    uint8_t *buf = (uint8_t *) malloc(numBytes);
+    memcpy(buf, data.data(), numBytes);
+    write(*sockets[socket_id], boost::asio::buffer(buf, numBytes), err);
+    free(buf);
+    //sockets[socket_id]->write_some(buffer(buf, numBytes), err);
+    if (!err) {
+        return true;
+    } else {
+        std::cout << "send failed: " << err.message() << '\n';
+        return false;
+    }
+}
 
 template<typename T>
 bool Network::rcv(uint party_id, T *data) {
@@ -88,7 +152,8 @@ bool Network::rcv(uint party_id, T *data) {
     }
     if (party_id > id) socket_id--;
     //boost::asio::streambuf recBuff;
-    boost::asio::mutable_buffer recBuff = buffer(data,sizeof(T)); //This is dangerous if a read_err occurred
+    boost::asio::mutable_buffer recBuff = boost::asio::buffer(data,
+                                                              sizeof(T)); //This is dangerous if a read_err occurred
     boost::system::error_code err;
     read(*sockets[socket_id], recBuff, boost::asio::transfer_at_least(sizeof(T)), err);
     if (err && err != boost::asio::error::eof) {
@@ -101,5 +166,35 @@ bool Network::rcv(uint party_id, T *data) {
     }
 }
 
+template<typename T>
+bool Network::rcv(uint party_id, std::vector<T> *data, uint32_t numElements) {
+    uint socket_id = party_id;
+    if (party_id > sockets.size() || party_id == id) {
+        std::cerr << "send failed! server id " << party_id << " is not legal" << '\n';
+        return false;
+    }
+    if (party_id > id) socket_id--;
+    //boost::asio::streambuf recBuff;
+    if (data->size() != numElements) {
+        std::cout << "vector size doesn't fit.\n";
+        return false;
+    }
+    uint32_t numBytes = numElements * sizeof(T);
+    //uint8_t* buf = (uint8_t*)malloc(numBytes);
+    //boost::asio::streambuf recBuff;
+    boost::asio::mutable_buffer recBuff = boost::asio::buffer(data->data(),numBytes); //This is dangerous if a read_err occurred
+    boost::system::error_code err;
+    read(*sockets[socket_id], recBuff, boost::asio::transfer_at_least(numBytes), err);
+    if (err && err != boost::asio::error::eof) {
+        std::cerr << "receive failed: " << err.message() << '\n';
+        return false;
+    } else {
+        //memcpy(data->data(), boost::asio::buffer_cast<const void *>(recBuff.data()), numBytes);
+        std::cout << "received.\n";
+        //free(buf);
+        return true;
+    }
+}
 
+void connect2player(uint8_t partyid, Network *player); //for threads
 #endif //MALICIOUS_PPML_NETWORK_H
