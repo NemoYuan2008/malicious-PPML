b8056b6e83bb4a5875bb7f75dfe16531d45d3d5b
Boshi Yuan <542629877@qq.com>
2023-06-06 16:36:41 +0800

    Add Party

---
+++ src/utils/Party.cpp
@@ -0,0 +1,4 @@
+#include "Party.h"
+
+
+
---
+++ src/utils/Party.h
@@ -0,0 +1,12 @@
+#ifndef MALICIOUS_PPML_PARTY_H
+#define MALICIOUS_PPML_PARTY_H
+
+
+struct Party {
+    const int myId;
+    const int partyNum;
+
+};
+
+
+#endif //MALICIOUS_PPML_PARTY_H
