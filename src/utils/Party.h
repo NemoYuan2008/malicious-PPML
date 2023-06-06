a25ec4c7de499e5f153c0ce003979f2a448b1b16
Boshi Yuan <542629877@qq.com>
2023-06-06 21:35:49 +0800

    Add fileStream to Party

--- src/utils/Party.h
+++ src/utils/Party.h
@@ -2,13 +2,18 @@
 #define MALICIOUS_PPML_PARTY_H
 
 
+#include <fstream>
+#include <string>
 #include <memory>
 #include "networking/Network.h"
 
 class Party {
 public:
-    Party(int myId, int partyNum, int port = 7766)
-            : myId(myId), partyNum(partyNum), network(myId, port) {
+    Party(int myId, int partyNum, const std::string &offlineFilePath, int port = 7766)
+            : myId(myId),
+              partyNum(partyNum),
+              fileStream(offlineFilePath),
+              network(myId, port) {
         network.connect();
     }
 
@@ -16,11 +21,18 @@ public:
 
     Party &operator=(const Party &) = delete;
 
+    int getMyId() const { return myId; }
+
+    int getPartyNum() const { return partyNum; }
+
+    std::ofstream &getFileStream() { return fileStream; }
+
     Network &getNetwork() { return network; }
 
 private:
-    int myId;
-    int partyNum;
+    const int myId;
+    const int partyNum;
+    std::ofstream fileStream;
     Network network;
 };
 
