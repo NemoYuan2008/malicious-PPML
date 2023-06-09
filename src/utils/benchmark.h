ac83712f378235fd3c07a221dd5118185c9e68ef
Boshi Yuan <542629877@qq.com>
2023-06-09 08:15:21 +0800

    Add benchmark.h

--- src/utils/benchmark.h
+++ src/utils/benchmark.h
@@ -3,11 +3,25 @@
 
 
 #include <chrono>
+#include <utility>
 
 template<typename Func, typename... Args>
 auto benchmark(const Func &f, Args &&...args) {
     auto start = std::chrono::steady_clock::now();
     f(std::forward<Args>(args)...);
+    auto end = std::chrono::steady_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    return duration.count();
+}
+
+
+template<typename Func, typename... Args>
+auto benchmark(size_t times, const Func &f, Args &&...args) {
+    auto start = std::chrono::steady_clock::now();
+    for (size_t i = 0; i < times; ++i) {
+        f(std::forward<Args>(args)...);
+    }
     auto end = std::chrono::steady_clock::now();
 
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
