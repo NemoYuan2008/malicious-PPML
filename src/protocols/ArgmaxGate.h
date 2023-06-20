c5495eda960b825a0b285c7120853437a7a3a8d1
Boshi Yuan <542629877@qq.com>
2023-06-20 15:04:38 +0800

    minor fixes

--- src/protocols/ArgmaxGate.h
+++ src/protocols/ArgmaxGate.h
@@ -1,9 +1,6 @@
-//
-// Created by ysx on 6/19/23.
-//
-
 #ifndef MALICIOUS_PPML_ARGMAXGATE_H
 #define MALICIOUS_PPML_ARGMAXGATE_H
+
 #include "protocols/Gate.h"
 #include "protocols/Gate.h"
 #include "utils/linear_algebra.h"
@@ -15,14 +12,14 @@ template<typename ShrType>
 class Circuit;
 
 template<typename ShrType>
-class ArgmxGate : public Gate<ShrType> {
+class ArgmaxGate : public Gate<ShrType> {
 public:
     using typename Gate<ShrType>::ClearType;
     using typename Gate<ShrType>::SemiShrType;
 
     using Gate<ShrType>::Gate;
 
-    ArgmxGate(const std::shared_ptr<Gate<ShrType>> &input_x)
+    explicit ArgmaxGate(const std::shared_ptr<Gate<ShrType>> &input_x)
             : Gate<ShrType>(input_x, nullptr), circuit(input_x->getParty()) {
 //        if (input_x->getDimRow() != input_y->getDimRow() || input_x->getDimCol() != input_y->getDimCol()) {
 //            throw std::logic_error("Dimension of the two inputs of addition don't match");
@@ -33,39 +30,38 @@ public:
         auto Delta = input_x->getDeltaClear();
         auto Lambda = input_x->getLambdaShr();
         uint32_t count = Delta.size() - 1;
-        std::vector<SemiShrType> indexShr(count+1,0);
+        std::vector<SemiShrType> indexShr(count + 1, 0);
         for (int i = 0; i < indexShr.size(); ++i) {
-            if(this->myId()==0) indexShr[i] = i;
+            if (this->myId() == 0) indexShr[i] = i;
         }
         //set dummy input gate
-        auto max = this->circuit.dummyInput(1,1);
+        auto max = this->circuit.dummyInput(1, 1);
         max.setDeltaClear({Delta[0]});//set max <-- delta[0]
         max.setLambdaShr({Lambda[0]});
-        auto maxInd = this->circuit.dummyInput(1,1); //set dummy input gate
-        if (this->myId()==0) {
+        auto maxInd = this->circuit.dummyInput(1, 1); //set dummy input gate
+        if (this->myId() == 0) {
             maxInd.setDeltaClear({0});//set max <-- delta[0]
         }
         maxInd.setLambdaClear({0});
         for (int i = 0; i < count; ++i) {
             //compare ret and x[i+1]
             //set dummy input gate
-            auto next = this->circuit.dummyInput(1,1);
-            next.setDeltaClear({Delta[i+1]});
-            next.setLambdaShr({Lambda[i+1]});
-            auto nextInd = this->circuit.dummyInput(1,1); //set dummy input gate
-            if (this->myId()==0) {
-                nextInd.setDeltaClear({i+1});//set max <-- delta[0]
-            }
-            else{
+            auto next = this->circuit.dummyInput(1, 1);
+            next.setDeltaClear({Delta[i + 1]});
+            next.setLambdaShr({Lambda[i + 1]});
+            auto nextInd = this->circuit.dummyInput(1, 1); //set dummy input gate
+            if (this->myId() == 0) {
+                nextInd.setDeltaClear({i + 1});//set max <-- delta[0]
+            } else {
                 nextInd.setDeltaClear({0});
             }
             nextInd.setLambdaShr({0});
             // compare max , next
             auto sub_ = this->circuit.subtract(max, next); // subtract: max - next
-            auto sub_Ind = this->circuit.subtract(maxInd,nextInd); // subtract
+            auto sub_Ind = this->circuit.subtract(maxInd, nextInd); // subtract
             auto b_ = this->circuit.gtz(); //: max-next > 0
-            auto product = this->circuit.elementMultiply(b_,sub_);
-            auto productInd = this->circuit.elementMultiply(b_,sub_Ind);
+            auto product = this->circuit.elementMultiply(b_, sub_);
+            auto productInd = this->circuit.elementMultiply(b_, sub_Ind);
             max = this->circuit.add(product, next); //max = b(max-next) + next
             maxInd = this->circuit.add(productInd, nextInd);
         }
@@ -80,7 +76,6 @@ private:
     void doRunOnline() override {
         this->deltaClear = matrixAdd(this->input_x->getDeltaClear(), this->input_y->getDeltaClear());
     }
-
 
 
     Circuit<ShrType> circuit;
