8c055a31043e842410bcedd09900105067ea3251
ysx <1512229747@qq.com>
2023-06-24 21:30:48 +0800

    change config

--- experiment/resnet18/resnet18Config.h
+++ experiment/resnet18/resnet18Config.h
@@ -21,8 +21,8 @@ void InitializeConv(){
     for (int i = 0; i < layers; ++i) {
         conv1_out_channels = expansion1[i]*conv1_in_channels;
         Conv1op[i].kernel_shape_ = {conv1_out_channels,conv1_in_channels,3,3};
-        Conv1op[i].input_shape_ = {conv1_in_channels,32,32};
-        Conv1op[i].output_shape_ = {conv1_out_channels,32,32};
+        Conv1op[i].input_shape_ = {conv1_in_channels,rows,cols};
+        Conv1op[i].output_shape_ = {conv1_out_channels,rows,cols};
         Conv1op[i].dilations_ ={1,1};
         Conv1op[i].pads_ ={1,1,1,1};
         Conv1op[i].strides_ ={1,1};
@@ -30,8 +30,8 @@ void InitializeConv(){
         conv2_in_channels = conv1_out_channels;
         conv2_out_channels = expansion2[i]*conv2_in_channels;
         Conv2op[i].kernel_shape_ = {conv2_out_channels,conv2_in_channels,3,3};
-        Conv2op[i].input_shape_ = {conv2_in_channels,32,32};
-        Conv2op[i].output_shape_ = {conv2_out_channels,32,32};
+        Conv2op[i].input_shape_ = {conv2_in_channels,rows,cols};
+        Conv2op[i].output_shape_ = {conv2_out_channels,rows,cols};
         Conv2op[i].dilations_ ={1,1};
         Conv2op[i].pads_ ={1,1,1,1};
         Conv2op[i].strides_ ={1,1};
@@ -39,8 +39,8 @@ void InitializeConv(){
         conv3_in_channels = conv1_in_channels;
         conv3_out_channels = expansion1[i]*expansion2[i]*conv1_in_channels;
         Conv3op[i].kernel_shape_ = {conv3_out_channels,conv3_in_channels,3,3};
-        Conv3op[i].input_shape_ = {conv3_in_channels,32,32};
-        Conv3op[i].output_shape_ = {conv3_out_channels,32,32};
+        Conv3op[i].input_shape_ = {conv3_in_channels,rows,cols};
+        Conv3op[i].output_shape_ = {conv3_out_channels,rows,cols};
         Conv3op[i].dilations_ ={1,1};
         Conv3op[i].pads_ ={1,1,1,1};
         Conv3op[i].strides_ ={1,1};
